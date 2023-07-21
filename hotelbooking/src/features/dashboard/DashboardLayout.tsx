5da9f22166b1d8323b4ce43cee6523db0ab96af6
dngocson <dngocson12@gmail.com>
2023-07-21 23:17:54 +0700

    building Dashboard
    building Dashboard

--- hotelbooking/src/features/dashboard/DashboardLayout.tsx
+++ hotelbooking/src/features/dashboard/DashboardLayout.tsx
@@ -6,3 +6,17 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+import React from "react";
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistic</div>
+      <div>Today's activity</div>
+      <div>Chart stay duration</div>
+      <div>Crat</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
